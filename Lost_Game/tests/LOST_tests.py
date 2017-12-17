3d6c503c5ac595bb09b23866331a560201838639
TheTiker <33525841+TheTiker@users.noreply.github.com>
2017-12-17 17:05:26 +0100

    Delete LOST_tests.py

--- Lost_Game/tests/LOST_tests.py
+++
@@ -1,11 +0,0 @@
-from nose.tools
-import *import NAME
-
-def setup():
-    print "Running setup for automated tests!"
-
-def teardown():
-    print "Running teardown for automated tests!"
-    
-def test_basic():
-    print "Running the first and only automated test!"
